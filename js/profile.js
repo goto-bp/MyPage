e62c5e2add3c007f46ed4aad0dbae0510c5f6210
goto-bp <81837715+goto-bp@users.noreply.github.com>
2023-07-24 11:15:16 +0900

    fixed breadcrumb link

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,14 @@
 function change_breadcrumb_news(name){
     let breadcrumb = document.getElementsByClassName('breadcrumb')[0];
-    breadcrumb.innerHTML = "<a href='profile.html'><img src='imgs/html_icon.png' class='file_icon'>\ profile.html\ </a> > <a href='#"+name+"'>"+name+"</a>";
+    let language = document.getElementsByTagName("html")[0].getAttribute("lang");
+
+    if(language == "jp")
+    {
+        breadcrumb.innerHTML = "<a href='profile.html'><img src='imgs/html_icon.png' class='file_icon'>\ profile.html\ </a> > <a href='#"+name+"'>"+name+"</a>";
+    }
+    else
+    {
+        breadcrumb.innerHTML = "<a href='profile.html'><img src='imgs/html_icon.png' class='file_icon'>\ profile_en.html\ </a> > <a href='#"+name+"'>"+name+"</a>";
+    }
     console.log(breadcrumb.innerHTML);
 };
